e637d16c037a39b6f767023e31e441fe3bb5c7ff
dharmik8991 <42092397+dharmik8991@users.noreply.github.com>
2018-12-14 03:08:23 +0530

    COMMIT

--- Assignments/Assignment-5/floating_point.s
+++ Assignments/Assignment-5/floating_point.s
@@ -3,49 +3,91 @@
 	 export __main	
 	 ENTRY 
 __main  function		 
-     VMOV.F32 S7,#1;//incrementer
+	 VMOV.F32 S7,#1;//incrementer
 	 B MAIN
+	 
+PRIN MOV R0,R8;
+	 BL printMsg;
+	 MOV R0,R9;
+	 BL printMsg;
+	 MOV R0,R10;
+	 BL printMsg;
+	 VCVTR.U32.F32 S9,S9;
+	 VMOV.F32 R0,S9;
+	 BL printMsg;
+	 ADD R8,R8,#1;
+	 SDIV R6,R8,R7;
+	 MUL R5,R6,R7;//FINDING MODULO
+	 SUB R8,R8,R5;
+	 
+	 ADD R9,R9,R6;
+	 SDIV R6,R9,R7;
+	 MUL R5,R6,R7;
+	 SUB R9,R9,R5;
+	 
+	 ADD R10,R10,R6;
+	 SDIV R6,R10,R7;
+	 MUL R5,R6,R7;
+	 SUB R10,R10,R5;
+	 VMOV.F32 S11,R8;
+	 VMOV.F32 S12,R9;
+	 VMOV.F32 S13,R10;
+	 VCVT.F32.U32 s11,S11;//A
+	 VCVT.F32.U32 s12,S12;//B
+	 VCVT.F32.U32 s13,S13;//C
+	 BX lr
 	 
 SIG	 VMOV.F32 s1,#1;//sigmoid subroutine
 	 VMOV.F32 s2,#1;//i
 	 VMOV.F32 s4,#1;
-	 VMOV.F32 r2,s1;
+	 VMOV.F32 R2,s1;
 loop VDIV.F32 s3,s0,s2;//finding x/i
 	 VMUL.F32 s4,s4,s3;//finding x^i/i=(x^i-1/(i-1))*i/n
 	 VADD.F32 s1,s1,s4;//s1(before)=1+x+...+x^i-1/(i-1) 
 	 VADD.F32 s2,s2,S7;
-	 VMOV.F32 r1,s1;
-	 CMP r1,r2;
-	 MOVNE r2,r1;
+	 VMOV.F32 R1,s1;
+	 CMP R1,R2;
+	 MOVNE R2,R1;
 	 BNE loop;
 	 VADD.F32 S8,S1,s7;
 	 VDIV.F32 S9,S1,S8;
-	 BX lr;
-MAIN VLDR.F32 s11,=1;//A
-	 VLDR.F32 s12,=1;//B
-	 VLDR.F32 s13,=0;//C
-	 VLDR.F32 s14,=1;//bias
-
+	 BX lr
+	 
+	 
+RESET MOV R0,#0;
+	  MOV R8,#0;
+	  MOV R9,#0;
+	  MOV R10,#0;
+	  MOV R6,#0;
+	  VLDR.F32 S11,=0;
+	  VLDR.F32 S12,=0;
+	  VLDR.F32 S13,=0;
+	  BX lr
+	  
+MAIN BL RESET
+	 VMOV.F32 s14,#1;//bias
+	 ;AND GATE
 	 VLDR.F32 s15,=2;//weight A
 	 VLDR.F32 s16,=2;//weight B
 	 VLDR.F32 s17,=2;//weight C
 	 VLDR.F32 s18,=-5;//weight bias
 	 
-	 VLDR.F32 S0,=0;//OUTPUT
-	 VFMA.F32 S0,S11,S15;
-	 VFMA.F32 S0,S12,S16;
-	 VFMA.F32 S0,S13,S17;
-	 VFMA.F32 S0,S14,S18;
-	 BL SIG
-	 VCVTR.S32.F32 s9,s9;
-	 VMOV.F32 r0,s9;
-	 BL printMsg;	 
-
+AND_L VLDR.F32 S0,=0;//OUTPUT
+	  VFMA.F32 S0,S11,S15;
+	  VFMA.F32 S0,S12,S16;
+	  VFMA.F32 S0,S13,S17;
+	  VFMA.F32 S0,S14,S18;
+	  BL SIG
+	  BL PRIN
+	  CMP R6,#1;
+	  BNE AND_L;
+	  BL RESET
+	  
 	 ;OR GATE
-	 VLDR.F32 s15,=2;//weight A
-	 VLDR.F32 s16,=2;//weight B
-	 VLDR.F32 s17,=2;//weight C
-	 VLDR.F32 s18,=-1;//weight bias
+OR_L	 VLDR.F32 s15,=2;//weight A
+		 VLDR.F32 s16,=2;//weight B
+		 VLDR.F32 s17,=2;//weight C
+		 VLDR.F32 s18,=-1;//weight bias
 	 
 	 VLDR.F32 S0,=0;//OUTPUT
 	 VFMA.F32 S0,S11,S15;
@@ -53,24 +95,31 @@ MAIN VLDR.F32 s11,=1;//A
 	 VFMA.F32 S0,S13,S17;
 	 VFMA.F32 S0,S14,S18;
 	 BL SIG
-	 VCVTR.S32.F32 s9,s9;
-	 VMOV.F32 r0,s9;
-	 BL printMsg;
+	  BL PRIN
+	 CMP R6,#1;
+	 BNE OR_L;
+	 BL RESET
 	 
 	 ;NOT GATE
-	 VLDR.F32 s15,=-2;//weight A
+	VLDR.F32 s15,=-2;//weight A
 	 VLDR.F32 s18,=1;//weight bias
 	 
-	 VLDR.F32 S0,=0;//OUTPUT
+NOT_L	 VLDR.F32 S0,=0;//OUTPUT
 	 VFMA.F32 S0,S11,S15;
 	 VFMA.F32 S0,S14,S18;
 	 BL SIG
-	 VCVTR.S32.F32 s9,s9;
-	 VMOV.F32 r0,s9;
+	 MOV R0,R8;
 	 BL printMsg;
- 	 
+	 VCVTR.U32.F32 S9,S9;
+	 VMOV.F32 R0,S9;
+	 BL printMsg;
+	 ADD R8,R8,#1;
+	 CMP R8,#1;
+	 BNE NOT_L;
+	 BL RESET
+	 	 
 	;XOR GATE
-	 VLDR.F32 s15, =-2.0
+XOR_L	  VLDR.F32 s15, =-2.0
      VLDR.F32 s16, =2.0
      VLDR.F32 s17, =-2.0
      VLDR.F32 s18, =-1.0
@@ -139,13 +188,13 @@ MAIN VLDR.F32 s11,=1;//A
 	 VFMA.F32 S0,S23,S18;
 	 VFMA.F32 s0,s14,s19;
 	 BL SIG
-	 VCVTR.S32.F32 s9,s9;
-     VMOV.F32 r0,s9
-     BL printMsg
-   
+	 BL PRIN
+	 CMP R6,#1;
+	 BNE XOR_L;
+	 BL RESET
 	 
 	 ;XNOR GATE
-	 VLDR.F32 s15, =2.0
+XNOR_L VLDR.F32 s15, =2.0
      VLDR.F32 s16, =-2.0
      VLDR.F32 s17, =2.0
      VLDR.F32 s18, =1.0
@@ -214,9 +263,10 @@ MAIN VLDR.F32 s11,=1;//A
 	 VFMA.F32 S0,S23,S18;
 	 VFMA.F32 s0,s14,s19;
 	 BL SIG
-	 VCVTR.S32.F32 s9,s9;
-     VMOV.F32 r0,s9
-     BL printMsg
+	 BL PRIN
+	 CMP R6,#1;
+	 BNE XNOR_L;
+	 BL RESET
 	 
 	 ;NAND GATE
 	 VLDR.F32 s15,=-2;//weight A
@@ -224,18 +274,19 @@ MAIN VLDR.F32 s11,=1;//A
 	 VLDR.F32 s17,=-2;//weight C
 	 VLDR.F32 s18,=5;//weight bias
 	 
-	 VLDR.F32 S0,=0;//OUTPUT
+NAND_L	 VLDR.F32 S0,=0;//OUTPUT
 	 VFMA.F32 S0,S11,S15;
 	 VFMA.F32 S0,S12,S16;
 	 VFMA.F32 S0,S13,S17;
 	 VFMA.F32 S0,S14,S18;
 	 BL SIG
-	 VCVTR.S32.F32 s9,s9;
-	 VMOV.F32 r0,s9;
-	 BL printMsg; 
+	  BL PRIN
+	 CMP R6,#1;
+	 BNE NAND_L;
+	 BL RESET
 	 
 	 ;NOR GATE
-	 VLDR.F32 s15,=-2;//weight A
+NOR_L	 VLDR.F32 s15,=-2;//weight A
 	 VLDR.F32 s16,=-2;//weight B
 	 VLDR.F32 s17,=-2;//weight C
 	 VLDR.F32 s18,=1;//weight bias
@@ -245,11 +296,10 @@ MAIN VLDR.F32 s11,=1;//A
 	 VFMA.F32 S0,S12,S16;
 	 VFMA.F32 S0,S13,S17;
 	 VFMA.F32 S0,S14,S18;
-	 BL SIG	;
-	 VCVTR.S32.F32 s9,s9;
-	 VMOV.F32 r0,s9;
-     BL printMsg;
-	 	 
+	 BL SIG
+	  BL PRIN
+	 CMP R6,#1;
+	 BNE NOR_L;
 fullstop    B  fullstop ; stop program	   
      endfunc
      end
